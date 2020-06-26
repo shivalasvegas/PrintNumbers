699f17e89451be2599dc860c53ea164cd4aa3f56
shivalasvegas <shiva.king@academytrainee.com>
2020-06-26 13:30:06 +0100

    Added to PrintNumbers class - printWordsTeens/Tens/Hundreds

--- src/qa/com/printnumbers/PrintNumbers.java
+++ src/qa/com/printnumbers/PrintNumbers.java
@@ -6,7 +6,10 @@ public class PrintNumbers {
 		
 		//printNumbersTen();
 		//printNumbersHun();
-		printWordsTen();
+		//printWordsUnits();
+		//printWordsTeens();
+		//printWordsTens();
+		printWordsHundreds();
 
 	}
 	
@@ -26,13 +29,108 @@ public class PrintNumbers {
 		}
 	}
 	
-	static void printWordsTen() {
-		String numberWords[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+	public static String numberWordsUnits[] = {" ", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
+	public static String numberWordsTeens[] = {"eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"};
+	public static String numberWordsTens[] = {"Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+
+	
+	static void printWordsUnits() {
 		
 		for (int i=0; i<10; i++) {
-			System.out.println(numberWords[i]);
+			System.out.println(numberWordsUnits[i]);
 		}
 	}
+	
+	static void printWordsTeens() {
+	
+		for (int i=0; i<10; i++) {
+			System.out.println(numberWordsUnits[i]);
+		}
+		
+		for (int i=0; i<9; i++) {
+			System.out.println(numberWordsTeens[i]);
+		}
+		
+		
+		
+	}
+	
+	static void printWordsTens() {
+		
+		for (int i=0; i<10; i++) {
+			System.out.println(numberWordsUnits[i]);
+		}
+		
+		for (int i=0; i<9; i++) {
+			System.out.println(numberWordsTeens[i]);
+		}
+	
+		for (int i=0; i<8; i++) {
+			//System.out.println(numberWordsTens[i]);				
+			for (int j=0; j<10; j++) {
+				System.out.println(numberWordsTens[i] + " " + numberWordsUnits[j]);	
+			}
+
+		}
+	}
+		
+		static void printWordsHundreds() {
+			// print out units
+			for (int i=0; i<10; i++) {
+				System.out.println(numberWordsUnits[i]);
+			}
+			
+			// print out teens
+			for (int i=0; i<9; i++) {
+				System.out.println(numberWordsTeens[i]);
+			}
+		
+			// print out tens
+			for (int i=0; i<8; i++) {
+				//System.out.println(numberWordsTens[i]);	
+				
+				for (int j=0; j<10; j++) {
+					System.out.println(numberWordsTens[i] + " " + numberWordsUnits[j]);	
+				}
+				
+			}
+			
+			//print out hundreds
+			
+			for (int i=0; i<10; i++) {
+			 // i = prefix to hundreds
+				
+				for (int j=0; j<10; j++) {
+					// j = units	
+					System.out.println(numberWordsUnits[i] + " hundred " + numberWordsUnits[j] );
+						
+				}	
+				
+				for (int j=0; j<9; j++) {
+					// j = teens	
+					System.out.println(numberWordsUnits[i] + " hundred " + numberWordsTeens[j] );
+						
+				}	
+				
+				for (int j=0; j<8; j++) {
+					// j = tens 
+					for (int k=0; k<10; k++) {
+						// k = units
+						System.out.println(numberWordsUnits[i] + " hundred " + numberWordsTens[j] + " " + numberWordsUnits[k]);	
+					}
+					
+				}
+				
+				
+			}
+			
+		
+		
+		
+	}
+	
+	
+	
 	
 	
 	

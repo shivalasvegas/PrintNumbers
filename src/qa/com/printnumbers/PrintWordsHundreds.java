dd6eb3395ff7c8a4b3c8993143a5e4b0614dafef
shivalasvegas <shiva.king@academytrainee.com>
2020-09-15 14:37:58 +0100

    SK - PrintNumbers: minor modifications.

--- src/qa/com/printnumbers/PrintWordsHundreds.java
+++ src/qa/com/printnumbers/PrintWordsHundreds.java
@@ -1,6 +1,7 @@
 package qa.com.printnumbers;
 
 
+
 public class PrintWordsHundreds {
 
 	public static String numberWordsUnits[] = {" ", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"};
